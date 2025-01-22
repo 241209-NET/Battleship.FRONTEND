a45819ef6613e96eef6957d2d42e30a479b2475d
prabveer <81586936+prabveer@users.noreply.github.com>
2025-01-22 08:03:32 -0600

    worked

--- battleship/src/App.jsx
+++ battleship/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import NavBar from './components/NavBar';
+import Home from './components/Home';
 import './App.css'
 
 function App() {
@@ -9,26 +9,17 @@ function App() {
 
   return (
     <>
+      <Router>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2 className="title">Battleship</h2>
+        <NavBar />
+        <div className="body">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </Router>
     </>
   )
 }
