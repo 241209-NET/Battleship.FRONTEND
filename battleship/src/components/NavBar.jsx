aed1a3dd568be112bea84dd5e7cf0ef774d7a677
prabveer <81586936+prabveer@users.noreply.github.com>
2025-01-22 09:07:56 -0600

    added nav bar and routing

--- battleship/src/components/NavBar.jsx
+++ battleship/src/components/NavBar.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
-
+import './NavBar.css';
 import { Link } from 'react-router-dom'
 export default function NavBar() {
     return (
         <nav className="navBar">
-            <div>
+            <div  className='Nav'>
                 <Link to="/login" className='link'>Login</Link>
                 <Link to="/signup" className='link'>Signup</Link>
                 <Link to="/home" className='link'>Home</Link>
