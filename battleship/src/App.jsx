c4a49b81ac6dd522f84b998dc403ee4b8f0809ce
Jean Valenzuela <Jean_Valenzuela2021@outlook.com>
2025-01-28 14:46:30 -0500

    Authorization and logout feat done

--- battleship/src/App.jsx
+++ battleship/src/App.jsx
@@ -3,13 +3,20 @@ import {
   Routes,
   Route,
   useLocation,
+  Navigate,
 } from "react-router"; // Correct import
 import Game from "./Pages/Game";
 import NavBar from "./components/NavBar";
 import Home from "./Pages/Home";
-import Waves from "./components/Waves"
-import Signin from './Pages/Signin';
+import Waves from "./components/Waves";
+import Signin from "./Pages/Signin";
+import NotFound from "./components/NotFound";
 import "./css/App.css";
+
+// Component to give access only to Authenticated Users
+const ProtectedRoute = ({ children }) => {
+  return sessionStorage.getItem("userid") ? children : <Navigate to="/" />;
+};
 
 // Component to manage conditional NavBar rendering
 function AppContent() {
@@ -23,11 +30,26 @@ function AppContent() {
       <div className="body">
         <Routes>
           <Route path="/" element={<Signin />} />
-          <Route path="/Home" element={<Home />} />
-          <Route path="/Game" element={<Game />} />
+          <Route
+            path="/Home"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/Game"
+            element={
+              <ProtectedRoute>
+                <Game />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
-      <Waves/>
+      <Waves />
     </div>
   );
 }
