6794af9f95c8abe9a9112681f9a0c4e59485318e
Ethan Catania <101900207+ethancatania@users.noreply.github.com>
2025-01-28 17:29:49 -0500

    Added Stats Leaderboard and Scoreboard components

---
+++ battleship/src/components/PlayerStats.jsx
@@ -0,0 +1,38 @@
+import { useState,useEffect } from 'react';
+import axios from 'axios';
+
+const PlayerStats = () => {
+    const [stats, setStats] = useState([]);
+    const tokenfromsesh = sessionStorage.getItem("token");
+
+    const getData = async () => {
+        try
+        {
+            const res = await axios.get('http://localhost:5298/Score', {
+                headers: {
+                    Authorization: `Bearer ${tokenfromsesh}`
+                }
+            });
+            setStats(res.data);
+            console.log(res.data);
+        } catch (e)
+        {
+            console.error("Could not fetch Data: " , e);
+        }
+        
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    return (
+        <div id="stats">
+            <h2>Player Stats</h2>
+                <h3>{stats.wins}</h3>
+                <h3>{stats.losses}</h3>
+        </div>
+);
+}
+
+export default PlayerStats;
