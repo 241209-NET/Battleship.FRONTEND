93fa7a6958a35d62ab85a7e10921fdeb7661b64a
Ethan Catania <101900207+ethancatania@users.noreply.github.com>
2025-01-30 15:52:27 -0500

    Update Home.jsx

--- battleship/src/Pages/Home.jsx
+++ battleship/src/Pages/Home.jsx
@@ -4,11 +4,6 @@ import { BarChart } from "@mui/x-charts/BarChart";
 import Scoreboard from "../components/Scoreboard";
 import GameList from "../components/GameList";
 import axios from "axios";
-<<<<<<< Updated upstream
-import './css/Home.css'
-=======
-
->>>>>>> Stashed changes
 
 export default function Home() {
   const [leaderboard, setLeaderboard] = useState(true);
